532673ee5c463eb2a77ab2966092eabe35c412b0
hamidrezakarami <hamidrezakarami16@gmail.com>
2021-10-18 01:30:01 +0330

    change KIND class

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,31 +1,40 @@
-import * as mongoose from 'mongoose';
-import { type } from 'os';
-import { of } from 'rxjs';
+import * as mongoose from 'mongoose'
 
-export const KIND =  ["car" , "cloth" , "electric"]
+export const KIND = [
+  'car',
+  'cloth',
+  'electric',
+  'laptop',
+  'smartPhone',
+  'art',
+  'sport',
+  'toy',
+  'dish',
+  'smartWatch',
+  'homeDesign'
+];
 
 export const ProductSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    expirationDate: { type: String, required: false },
-    productionDate: { type: String, required: false },
-    price: { type: Number, required: true },
-    numberOfEntity: { type: Number, required: false },
-    description: { type: String, required: false },
-    imgUrls: { type: Array, required: false }, // 1. Admin  2. Employee  3. Customer
-    kind: { type : KIND , required: false },
-    imgUrl: { type: String, required: false },
+  name: { type: String, required: true },
+  expirationDate: { type: String, required: false },
+  productionDate: { type: String, required: false },
+  price: { type: Number, required: true },
+  numberOfEntity: { type: Number, required: false },
+  description: { type: String, required: false },
+  imgUrls: { type: Array, required: false }, // 1. Admin  2. Employee  3. Customer
+  kind: { type: KIND, required: false },
+  imgUrl: { type: String, required: false },
 });
 
-export interface Product extends mongoose.Document 
-{
-    id: string;
-    name: string;
-    expirationDate: string;
-    productionDate: string;
-    price: number;
-    numberOfEntity: number;
-    description: string;
-    imgUrls: Array<string>;
-    kind: typeof KIND  ;
-    imgUrl: string;
+export interface Product extends mongoose.Document {
+  id: string;
+  name: string;
+  expirationDate: string;
+  productionDate: string;
+  price: number;
+  numberOfEntity: number;
+  description: string;
+  imgUrls: Array<string>;
+  kind: typeof KIND;
+  imgUrl: string;
 }
